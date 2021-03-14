7e2bbeeab69cbf6627a7eb908739e060673fd2f9
rshero <52062460+rshero@users.noreply.github.com>
2021-03-14 19:42:25 +0530

    Update anime_quotes.py
    Updated API

--- YuiiChan/modules/anime_quotes.py
+++ YuiiChan/modules/anime_quotes.py
@@ -1,32 +1,23 @@
 import json
 import requests
-from YuiiChan import dispatcher
-from YuiiChan.modules.disable import DisableAbleCommandHandler
-from telegram import (
-    ParseMode,
-    Update,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    replymarkup,
-)
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update, InlineKeyboardMarkup, InlineKeyboardButton, replymarkup
 from telegram.error import BadRequest
-from telegram.ext import (
-    CallbackContext,
-    CommandHandler,
-    Filters,
-    run_async,
-    CallbackQueryHandler,
-)
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async, CallbackQueryHandler
 
 
 def anime_quote():
-    url = "https://animechanapi.xyz/api/quotes/random"
-    response = requests.get(url)
+    url = "https://animechan.vercel.app/api/random"
     # since text attribute returns dictionary like string
-    dic = json.loads(response.text)
-    quote = dic["data"][0]["quote"]
-    character = dic["data"][0]["character"]
-    anime = dic["data"][0]["anime"]
+    response = requests.get(url)
+    try:
+        dic = json.loads(response.text)
+    except Exception:
+        pass
+    quote = dic["quote"]
+    character = dic["character"]
+    anime = dic["anime"]
     return quote, character, anime
 
 
@@ -35,9 +26,10 @@ def quotes(update: Update, context: CallbackContext):
     message = update.effective_message
     quote, character, anime = anime_quote()
     msg = f"<i>❝{quote}❞</i>\n\n<b>{character} from {anime}</b>"
-    keyboard = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text="Change🔁", callback_data="change_quote")]]
-    )
+    keyboard = InlineKeyboardMarkup([[
+        InlineKeyboardButton(
+            text="Change🔁",
+            callback_data="change_quote")]])
     message.reply_text(
         msg,
         reply_markup=keyboard,
@@ -52,15 +44,19 @@ def change_quote(update: Update, context: CallbackContext):
     message = update.effective_message
     quote, character, anime = anime_quote()
     msg = f"<i>❝{quote}❞</i>\n\n<b>{character} from {anime}</b>"
-    keyboard = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text="Change🔁", callback_data="quote_change")]]
-    )
-    message.edit_text(msg, reply_markup=keyboard, parse_mode=ParseMode.HTML)
+    keyboard = InlineKeyboardMarkup([[
+        InlineKeyboardButton(
+            text="Change🔁",
+            callback_data="quote_change")]])
+    message.edit_text(msg, reply_markup=keyboard,
+                      parse_mode=ParseMode.HTML)
 
 
 QUOTE = DisableAbleCommandHandler("quote", quotes)
-CHANGE_QUOTE = CallbackQueryHandler(change_quote, pattern=r"change_.*")
-QUOTE_CHANGE = CallbackQueryHandler(change_quote, pattern=r"quote_.*")
+CHANGE_QUOTE = CallbackQueryHandler(
+    change_quote, pattern=r"change_.*")
+QUOTE_CHANGE = CallbackQueryHandler(
+    change_quote, pattern=r"quote_.*")
 
 dispatcher.add_handler(QUOTE)
 dispatcher.add_handler(CHANGE_QUOTE)
